7c3dfe37a530bb8085cefbeedd5d2abf489e4fc9
rickshev <rick.shevlin@gmail.com>
2021-03-08 22:56:10 -0500

    Started javascript app

---
+++ Football_App/static/app.js
@@ -0,0 +1,8 @@
+// import data from data.js
+var totalData = data;
+var qbData = qbdata;
+var rbData = rbdata;
+var wrData = wrdata;
+var teData = tedata;
+
+console.log(totalData);
---
+++ Football_App/static/data.js
@@ -0,0 +1,5 @@
+var data = $.csv.toObjects("../Data/fantasy.csv");
+var qbdata = $.csv.toObjects("../Data/qb.csv");
+var rbdata = $.csv.toObjects("../Data/rb.csv");
+var wrdata = $.csv.toObjects("../Data/wr.csv");
+var tedata = $.csv.toObjects("../Data/te.csv");
